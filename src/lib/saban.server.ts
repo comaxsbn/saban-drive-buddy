c631913a626599c3dc686f15c31334fd69b38b76
comaxsbn <comax.sbn@gmail.com>
2026-08-16 16:42:59 +0300

    Update saban.server.ts

--- src/lib/saban.server.ts
+++ src/lib/saban.server.ts
@@ -44,7 +44,7 @@ export class SabanServerCore {
   private config: SabanServerConfig = {
     gasUrl:
       (typeof process !== 'undefined' && (process.env.VITE_GAS_URL || process.env.VITE_GAS_URL_GEMINI)) ||
-      'https://script.google.com/macros/s/AKfycbwJDwvb3wPYFoCEVCH_E9Mdco5w_dZeh35KNtCJB8_GdSllt59vV10oWgEA-QaH4S5A/exec',
+      'https://script.google.com/macros/s/AKfycbxaP0WMAEJZu7PkHWFnVFLKfgbgeimbrHQBQ6FYNsqTgsAHCOtw5c555dIqUysnbQFUJw/exec',
     spreadsheetId:
       (typeof process !== 'undefined' && process.env.SABAN_SPREADSHEET_ID) ||
       '1dcQ4TfEZzIOvGLqK3arRiVpyU2l6UM7jcvS1QW_b3Lg',
